8d8e7ca6897016732f3f8bf011a9fa37834f441a
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-11-18 04:57:19 -0800

    fix: navigation and minor clean up

--- src/app/_components/bottom-nav.tsx
+++ src/app/_components/bottom-nav.tsx
@@ -1,8 +1,8 @@
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export function BottomNav({ children }: { children?: ReactNode }) {
   return (
-    <nav className="bg-background text-primary fixed bottom-0 mx-auto w-full max-w-lg border-t-2 pb-3 pt-2">
+    <nav className="bg-background text-primary sticky bottom-0 mx-auto w-full max-w-lg border-t-2 pb-3 pt-2">
       {children}
     </nav>
   );
--- src/app/_components/navigation.tsx
+++ src/app/_components/navigation.tsx
@@ -11,53 +11,45 @@ import { usePathname } from "next/navigation";
 export function Navigation() {
   const pathname = usePathname();
 
-  console.log(pathname);
-
   return (
     <ul className="flex flex-row justify-around">
-      <li
-        className={`flex flex-col items-center justify-center gap-1 ${
-          pathname === "/" ? "text-primary" : "text-gray-400"
-        }`}
-      >
+      <li>
         <Link
           href="/"
-          className="1ext-xs flex cursor-pointer flex-col items-center gap-2 uppercase"
+          className={`flex flex-col items-center justify-center gap-1 ${
+            pathname === "/" ? "text-primary" : "text-gray-400"
+          }`}
         >
           <div className="flex aspect-square items-center justify-center">
             <HomeIcon className="w-7" />
           </div>
-          홈
+          <span className="text-xs uppercase">홈</span>
         </Link>
       </li>
-      <li
-        className={`flex flex-col items-center justify-center gap-1 ${
-          pathname === "/search" ? "text-primary" : "text-gray-400"
-        }`}
-      >
+      <li>
         <Link
           href="/search"
-          className="flex cursor-pointer flex-col items-center gap-1 text-xs uppercase"
+          className={`flex flex-col items-center justify-center gap-1 ${
+            pathname === "/search" ? "text-primary" : "text-gray-400"
+          }`}
         >
           <div className="flex aspect-square  items-center justify-center">
             <MagnifyingGlassIcon className="w-7" />
           </div>
-          검색
+          <div className="text-xs uppercase">검색</div>
         </Link>
       </li>
-      <li
-        className={`flex flex-col items-center justify-center gap-1 ${
-          pathname === "/mypage" ? "text-primary" : "text-gray-400"
-        }`}
-      >
+      <li>
         <Link
           href="/mypage"
-          className="flex cursor-pointer flex-col items-center gap-1 text-xs uppercase"
+          className={`flex flex-col items-center justify-center gap-1 ${
+            pathname === "/mypage" ? "text-primary" : "text-gray-400"
+          }`}
         >
           <div className="flex aspect-square  items-center justify-center">
             <UserIcon className="w-7" />
           </div>
-          마이페이지
+          <div className="text-xs uppercase">마이페이지</div>
         </Link>
       </li>
     </ul>
