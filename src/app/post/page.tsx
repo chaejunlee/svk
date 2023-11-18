66afb5f45a6602060349ca87f36a36b105ca2e71
Jooyul Yoon <juyeolyoon@gmail.com>
2023-11-17 23:28:31 -0800

    post page

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,5 +1,4 @@
 import { getServerAuthSession } from "@/server/auth";
-import styles from "./post.module.css";
 import {
   ChevronLeftIcon,
   BookmarkIcon,
@@ -9,7 +8,6 @@ import {
   GlobeAsiaAustraliaIcon,
   QrCodeIcon,
 } from "@heroicons/react/20/solid";
-import Image from "next/image";
 
 export default async function Page() {
   const session = await getServerAuthSession();
@@ -47,28 +45,59 @@ export default async function Page() {
           <div className="h-2 w-2 rounded bg-slate-400"></div>
         </div>
       </div>
-      <div className=" border-bottom-2 flex justify-between border-solid border-sky-500 px-10 py-4">
+      <div className="flex justify-between border-b-2 border-slate-500  px-10 py-4">
         <div className="w-20 text-center">Overview</div>
         <div className="w-20 text-center">Menu</div>
         <div className="w-20 text-center">Pictures</div>
       </div>
-      <div className="flex flex-col px-6">
+      <div className="my-10 flex flex-col px-6">
         <div>미용/네일/카테고리</div>
-        <div>closed now</div>
+        <div className="my-10">closed now</div>
         <div className="flex justify-between px-4">
-          <div className="flex flex-col justify-center">
-            <div className=" flex h-12 w-12 bg-slate-700">
-              <PhoneIcon width={20} />
+          <div className="flex flex-col items-center justify-center">
+            <div className=" flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-slate-200 hover:bg-slate-300">
+              <PhoneIcon width={30} />
             </div>
             <div>Call</div>
           </div>
-          <div>Call</div>
-          <div>Call</div>
-          <div>Call</div>
+          <div className="flex flex-col items-center justify-center">
+            <div className=" flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-slate-200 hover:bg-slate-300">
+              <GlobeAsiaAustraliaIcon width={30} />
+            </div>
+            <div>Website</div>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <div className=" flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-slate-200 hover:bg-slate-300">
+              <ShareIcon width={30} />
+            </div>
+            <div>Share</div>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <div className=" flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-slate-200 hover:bg-slate-300">
+              <QrCodeIcon width={30} />
+            </div>
+            <div>Reserve</div>
+          </div>
         </div>
-        <div>MAP</div>
+        <div className="my-10 h-96 w-full bg-slate-200">Map</div>
+        <div>
+          <div className="text-3xl font-bold">Reviews</div>
+          <div className="mt-6 flex items-center gap-10">
+            <div>
+              <div className="text-sm">Overall rating</div>
+              <div className="text-lg">⭐️⭐️⭐️⭐️⭐️</div>
+              <div className="text-sm">44 Reviews</div>
+            </div>
+            <div>
+              <div className="text-sm">5</div>
+              <div className="text-sm">4</div>
+              <div className="text-sm">3</div>
+              <div className="text-sm">2</div>
+              <div className="text-sm">1</div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>Reviews</div>
     </main>
   );
 }
