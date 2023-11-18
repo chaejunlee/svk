d34d00397a592362735cf40e434dc60730e55b94
Jooyul Yoon <juyeolyoon@gmail.com>
2023-11-18 03:37:17 -0800

    style: post layout

--- src/app/post/layout.tsx
+++ src/app/post/layout.tsx
@@ -1,7 +1,30 @@
+import Link from "next/link";
+import { BottomNav } from "../page";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      <BottomNav>
+        <Booking />
+      </BottomNav>
+    </>
+  );
+}
+
+function Booking() {
+  return (
+    <div className="flex px-2">
+      <Link
+        href="/book"
+        className="flex h-12 w-full items-center justify-center rounded-lg bg-primary font-semibold text-white no-underline transition hover:bg-primary/80"
+      >
+        예약하기
+      </Link>
+    </div>
+  );
 }
