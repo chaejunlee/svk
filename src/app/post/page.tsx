687717955378c18908d3ebfae8f283118310fe0f
Jooyul Yoon <juyeolyoon@gmail.com>
2023-11-17 21:02:52 -0800

    post page created

---
+++ src/app/post/page.tsx
@@ -0,0 +1,15 @@
+import { getServerAuthSession } from "@/server/auth";
+import styles from "./post.module.css";
+
+export default async function Page() {
+  const session = await getServerAuthSession();
+
+  return (
+    <main className="relative mx-auto flex min-h-[100svh] max-w-lg flex-col border-2 bg-gray-50">
+      <div className={styles.header}>
+        <div className={styles.header_left}>left</div>
+        <div className={styles.header_left}>right</div>
+      </div>
+    </main>
+  );
+}
